122e175111aa04b366d2215604af3a21422ebba9
muhammad.fawad <m.fawad.996@gmail.com>
2022-01-27 16:05:36 +0500

    Initial Committ

--- app/src/main/java/com/example/mytestproject/MainActivity.java
+++ app/src/main/java/com/example/mytestproject/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String abc="1234";
+        String hello = "Hello World";
     }
 }
